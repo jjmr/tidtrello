6da5cd7f6b04aa26655c376883406110e626126d
Jimmy Collazos <acido69@gmail.com>
2013-11-12 00:28:08 +0100

    addTest - test6

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,9 +3,21 @@
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('my app', function() {
-
-  beforeEach(function() {
-    browser().navigateTo('index.html');
-  });
+    describe('TODO column', function(){
+        beforeEach(function() {
+            browser().navigateTo('index.html');
+        });
+        it('put card in his column', function(){
+            expect(repeater('#column-TODO .card').count()).toBe(2);
+        });
+    });
+    describe('DOING column', function(){
+        beforeEach(function() {
+            browser().navigateTo('index.html');
+        });
+        it('put card in his column', function(){
+            expect(repeater('#column-DOING .card').count()).toBe(1);
+        });
+    });
 
 });

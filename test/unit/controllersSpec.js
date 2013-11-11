b34bc1dad80cc9e76fee49722fda465ade9336fd
Jimmy Collazos <acido69@gmail.com>
2013-11-11 20:30:36 +0100

    addTest - test2

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,16 +6,29 @@ describe('controllers', function() {
     beforeEach(function(){
         module('tidtrello');
     });
-    describe('CardsController', function(){
-      var scope, ctrl;
+    describe('CardsController :: ', function(){
+      var scope, ctrl, CardsInstance, $httpBackend;
 
-      beforeEach(inject(function($rootScope, $controller) {
+      beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, Cards) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('/cards').
+            respond([]);
+
+
         scope = $rootScope.$new();
-        ctrl = $controller('CardsController', {$scope: scope});
+        CardsInstance = Cards;
+        ctrl = $controller('CardsController', {$scope: scope, Cards: Cards});
       }));
 
       it('my columns is defined', function() {
         expect(scope.columns).toBeDefined();
       });
+
+      it('my cards is defined', function() {
+        $httpBackend.flush();
+        expect(scope.cards).toBeDefined();
+        expect(scope.cards.length).toEqual(0);
+      });
+
     });
 });
